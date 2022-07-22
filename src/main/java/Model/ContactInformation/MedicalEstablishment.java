7f3a5f431f3bc8eae4c899b96975e93716db0032
Alexandre Filion <filion.alexandre.2@courrier.uqam.ca>
2022-07-22 16:34:11 -0400

    MedicalEstablishment : classe et tests

--- src/main/java/Model/ContactInformation/MedicalEstablishment.java
+++ src/main/java/Model/ContactInformation/MedicalEstablishment.java
@@ -2,9 +2,10 @@ package Model.ContactInformation;
 
 public class MedicalEstablishment {
 
-    public MedicalEstablishment(int establishmentId, String name, ContactInformation contactInformation) {
-        this.establishmentId = establishmentId;
-        this.name = name;
+    public MedicalEstablishment(int establishmentId, String name,
+                                ContactInformation contactInformation) {
+        validateEstablishmentId(establishmentId);
+        validateName(name);
         addContactInformation(contactInformation);
     }
 
@@ -16,20 +17,36 @@ public class MedicalEstablishment {
         return establishmentId;
     }
 
-//    public void setEstablishmentId(int establishmentId) {
-//        this.establishmentId = establishmentId;
-//    }
-
     public String getName() {
         return name;
     }
 
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-
     public ContactInformation getContactInformation() {
         return contactInformation;
+    }
+
+    private void validateEstablishmentId(int establishmentId) {
+        // Valid establishmentId example : 2022
+        // Invalid establishmentId examples : MTL-2022, MTL2002
+        if (establishmentId >= 1000 && establishmentId <= 9999)
+            this.establishmentId = establishmentId;
+        else
+            this.establishmentId = 0;
+    }
+
+    private void validateName(String name) {
+        // Valid name example : Pierre-Boucher Hospital, CHUM
+        // Invalid name examples : Hospital 1
+        boolean isValid = false;
+        String validFormat = "^[A-Z]+([a-zA-Z-\\s]+)*$";
+
+        if (name.matches(validFormat))
+            isValid = true;
+
+        if (!isValid)
+            this.name = null;
+        else
+            this.name = name;
     }
 
     private void addContactInformation(ContactInformation contactInformation) {
