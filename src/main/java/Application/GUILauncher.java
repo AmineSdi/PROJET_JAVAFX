168231e3d6f3a36414cf71b539b01b1e824497cb
ad791042 <ad791042@ens.uqam.ca>
2022-07-23 16:29:09 -0400

    Modifie les fichiers fxml addVisit et addHistory

--- src/main/java/Application/GUILauncher.java
+++ src/main/java/Application/GUILauncher.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class GUILauncher extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("session.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800.0, 416.0);
         stage.setTitle("Contacts");
         stage.setScene(scene);
