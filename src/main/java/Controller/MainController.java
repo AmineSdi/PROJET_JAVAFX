df39c0acd73b91e3de627e576d2bd876573578fd
he091019 <goulet.nicolas@courrier.uqam.ca>
2022-07-27 16:01:06 -0400

    Fix before merge

--- src/main/java/Controller/MainController.java
+++ src/main/java/Controller/MainController.java
@@ -6,7 +6,6 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.Objects;
 import java.util.ResourceBundle;
-import Controller.SceneController;
 import java.util.Date;
 import Model.PatientFile.Gender;
 import Model.PatientFile.MedicalHistory;
@@ -232,107 +231,10 @@ public class MainController implements Initializable {
         window.show();
     }
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
         // showPatientFiles();
     }
-
-
-
-    public Connection getConnection() {
-        String jdbcUrl = "jdbc:sqlite:MedicalSystem.db";
-
-        try {
-            Connection connection = DriverManager.getConnection(jdbcUrl);
-            return connection;
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-            return null;
-        }
-    }
-
-
-
-
-
-
-    //******************************************************//
-    //EVERYTHING UNDER THIS HAS TO BE FIXED BEFORE MERGE!!!!//
-    //******************************************************//
-    public ObservableList<PatientFile> getPatientFiles() {
-        ObservableList<PatientFile> patientFiles = FXCollections.observableArrayList();
-        Connection conn = getConnection();
-        String query = "SELECT * FROM PatientFiles";
-        Statement statement;
-        ResultSet resultSet;
-        try {
-            statement = conn.createStatement();
-            resultSet = statement.executeQuery(query);
-            PatientFile file;
-            while(resultSet.next()) {
-                file = new PatientFile(
-                    resultSet.getString("ramqCode"),
-                    resultSet.getString("firstName"),
-                    resultSet.getString("lastName"),
-                    Gender.FEMALE,
-                    LocalDate.parse(resultSet.getString("birthDate")),
-                    resultSet.getString("birthCity"),
-                    resultSet.getString("parentsName"),
-                    new ContactInformation(1, null, null, null, null, null)); // Todo: fix
-                patientFiles.add(file);
-            }
-
-        } catch(Exception ex) {
-            ex.printStackTrace();
-        }
-        return patientFiles;
-    }
-
-    // public void showMedicalVisit(){
-    //     ObservableList<MedicalVisit> list = getPatientFiles();
-
-    //     colRamqCode.setCellValueFactory(new PropertyValueFactory<PatientFile, String>("ramqCode"));
-    //     colLastName.setCellValueFactory(new PropertyValueFactory<PatientFile, String>("firstName"));
-    //     colFirstName.setCellValueFactory(new PropertyValueFactory<PatientFile, String>("lastName"));
-
-    //     tvMedicalVisit.setItems(list);
-    // }
-    // private void insertRecord(){
-    //     String query = "INSERT INTO PatientFiles VALUES (" + tfRamqCode.getText() + ",'" + tfFirstName.getText() + "','" + tfLastName.getText() + "')";
-    //     executeQuery(query);
-    //     showPatientFiles();
-    // }
-    // private void updateRecord(){
-    //     String query = "UPDATE PatientFiles SET firstName  = '" + tfFirstName.getText() + "', lastName = '" + tfLastName.getText() + "' WHERE ramqCode = " + tfRamqCode.getText() + "";
-    //     executeQuery(query);
-    //     showPatientFiles();
-    // }
-    // private void deleteButton(){
-    //     String query = "DELETE FROM PatientFiles WHERE ramqCode =" + tfRamqCode.getText() + "";
-    //     executeQuery(query);
-    //     showPatientFiles();
-    // }
-
-    // private void submitButton(){
-
-    //     String query = "SELECT FROM PatientFiles WHERE ramqCode =" + tfRamqCode.getText() + "";
-    //     executeQuery(query);
-    //     showPatientFiles();
-    // }
-
-    // private void executeQuery(String query) {
-    //     Connection conn = getConnection();
-    //     Statement st;
-    //     try{
-    //         st = conn.createStatement();
-    //         st.executeUpdate(query);
-    //     }catch(Exception ex){
-    //         ex.printStackTrace();
-    //     }
-    // }
 
 
 }
