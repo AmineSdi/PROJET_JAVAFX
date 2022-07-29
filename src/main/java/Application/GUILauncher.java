27362a0d69155aab24860c8570c66632c8622ce2
Alexandre Filion <filion.alexandre.2@courrier.uqam.ca>
2022-07-29 15:16:59 -0400

    Ajoute une méthode pour le bouton X du GUI
    Permet de personnaliser la fermeture du programme

--- src/main/java/Application/GUILauncher.java
+++ src/main/java/Application/GUILauncher.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -19,6 +21,22 @@ public class GUILauncher extends Application {
         //stage.setTitle("Consultation des historiques médicaux");
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logoutMedicalProgram(stage);
+        });
+    }
+
+    public void logoutMedicalProgram(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Medical program");
+        alert.setHeaderText("Would you like to exit the Medical program?");
+        alert.setContentText("Click yes to exit, click cancel to return");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            //Code à effectuer lorsque le programme se termine
+            stage.close();
+        }
     }
 
     public static void launch(String[] args) {
