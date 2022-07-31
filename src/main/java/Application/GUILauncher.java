42b5b449eda1093c9c3f48673d6a94cd69fe60b1
he091019 <goulet.nicolas@courrier.uqam.ca>
2022-07-31 11:05:53 -0400

    Corrige les conflits avant merge

--- src/main/java/Application/GUILauncher.java
+++ src/main/java/Application/GUILauncher.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -27,13 +29,22 @@ public class GUILauncher extends Application {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            logoutMedicalProgram(stage);
+        });
+    }
 
-//        URL url = new File("src/main/resources/Application/login.fxml").toURI().toURL();
-//        Parent root = FXMLLoader.load(url);
-//        Scene scene = new Scene(root);
-//        //stage.setTitle("Consultation des historiques médicaux");
-//        stage.setScene(scene);
-//        stage.show();
+    public void logoutMedicalProgram(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Medical program");
+        alert.setHeaderText("Would you like to exit the Medical program?");
+        alert.setContentText("Click yes to exit, click cancel to return");
+
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            //Code à effectuer lorsque le programme se termine
+            stage.close();
+        }
     }
 
     public static void launch(String[] args) {
