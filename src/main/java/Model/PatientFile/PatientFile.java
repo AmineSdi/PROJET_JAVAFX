ffa2aaa24ffef6a379b675c999e142a9feae58b5
Alexandre Filion <filion.alexandre.2@courrier.uqam.ca>
2022-08-02 14:11:41 -0400

    Mettre à jour la classe PatientFile
    Inclus les tests pour cette classe

--- src/main/java/Model/PatientFile/PatientFile.java
+++ src/main/java/Model/PatientFile/PatientFile.java
@@ -12,13 +12,13 @@ public class PatientFile {
     // TODO : Private constructor for builder pattern?
     public PatientFile(String ramqCode, String firstName, String lastName, Gender gender,
                        String birthCity, LocalDate birthDate, String knownParents) {
-        this.ramqCode = ramqCode;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.gender = gender;
-        this.birthCity = birthCity;
-        this.birthDate = birthDate;
-        this.knownParents = knownParents;
+        validateRamqCode(ramqCode);
+        validateFirstName(firstName);
+        validateLastName(lastName);
+        validateGender(gender);
+        validateBirthCity(birthCity);
+        validateBirthDate(birthDate);
+        validateKnownParents(knownParents);
     }
 
     String ramqCode;
@@ -44,56 +44,130 @@ public class PatientFile {
         return ramqCode;
     }
 
-    public void setRamqCode(String ramqCode) {
-        this.ramqCode = ramqCode;
+    public void validateRamqCode(String ramqCode) {
+        // Valid ramqCode example : ABCD11223344
+        // Invalid ramqCodee example : ABCD 11223344
+        boolean isValid = false;
+        String validFormat = "^[A-Z]{4}\\d{8}$";
+
+        if (ramqCode.matches(validFormat))
+            isValid = true;
+
+        if (!isValid)
+            this.ramqCode = null;
+        else
+            this.ramqCode = ramqCode;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void validateFirstName(String firstName) {
+        // Valid firstName example : any first name without number
+        // Invalid firstName example : any number in the first name or empty string
+        boolean isValid = false;
+        String validFormat = "^([a-zA-Z-\\s])*$";
+
+        if (firstName.matches(validFormat) && firstName.length() > 0)
+            isValid = true;
+
+        if (!isValid)
+            this.firstName = null;
+        else
+            this.firstName = firstName;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void validateLastName(String lastName) {
+        // Valid lastName example : any last name without number
+        // Invalid lastName example : any number in the last name or empty string
+        boolean isValid = false;
+        String validFormat = "^([a-zA-Z-\\s])*$";
+
+        if (lastName.matches(validFormat) && lastName.length() > 0)
+            isValid = true;
+
+        if (!isValid)
+            this.lastName = null;
+        else
+            this.lastName = lastName;
     }
 
     public Gender getGender() {
         return gender;
     }
 
-    public void setGender(Gender gender) {
-        this.gender = gender;
+    public void validateGender(Gender gender) {
+        // Valid lastName example : any last name without number
+        // Invalid lastName example : any number in the last name or empty string
+        boolean isValid = gender.equals(Gender.MALE) || gender.equals(Gender.FEMALE)
+                || gender.equals(Gender.OTHER);
+
+        if (!isValid)
+            this.gender = null;
+        else
+            this.gender = gender;
     }
 
     public LocalDate getBirthDate() {
         return birthDate;
     }
 
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
+    public void validateBirthDate(LocalDate birthDate) {
+        // Valid birthDate example :
+        // Invalid birthDate examples :
+        boolean isValid = false;
+
+        LocalDate date = LocalDate.now();
+        if (birthDate.equals(date))
+            isValid = true;
+
+        if (!isValid)
+            this.birthDate = null;
+        else
+            this.birthDate = birthDate;
     }
 
     public String getBirthCity() {
         return birthCity;
     }
 
-    public void setBirthCity(String birthCity) {
-        this.birthCity = birthCity;
+    public void validateBirthCity(String birthCity) {
+        // Valid street example : Montreal
+        // Invalid street example : Montréal or empty string
+        boolean isValid = false;
+        String validFormat = "^([a-zA-Z-\\s])*$";
+
+        if (birthCity.matches(validFormat) && birthCity.length() > 0)
+            isValid = true;
+
+        if (!isValid)
+            this.birthCity = null;
+        else
+            this.birthCity = birthCity;
     }
 
     public String getKnownParents() {
         return knownParents;
     }
 
-    public void setKnownParents(String knownParents) {
-        this.knownParents = knownParents;
+    public void validateKnownParents(String knownParents) {
+        // Valid street example : any name(s) without number
+        // Invalid street example : any name(s) with number or empty string
+        boolean isValid = false;
+        String validFormat = "^([a-zA-Z-,\\s])*$";
+
+        if (knownParents.matches(validFormat) && knownParents.length() > 0)
+            isValid = true;
+
+        if (!isValid)
+            this.knownParents = null;
+        else
+            this.knownParents = knownParents;
     }
 
     public ContactInformation getContactInformation() {
---
+++ src/test/java/PatientFileTest.java
@@ -0,0 +1,149 @@
+import Model.ContactInformation.ContactInformation;
+import Model.PatientFile.Gender;
+import Model.PatientFile.MedicalHistory;
+import Model.PatientFile.MedicalVisit;
+import Model.PatientFile.PatientFile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PatientFileTest {
+
+    PatientFile pf1, pf2, pf3, pf4, pf5, pf6, pf7;
+    MedicalHistory mh;
+    MedicalVisit mv;
+    LocalDate startDate = LocalDate.now();
+    LocalDate visitDate = LocalDate.now();
+    LocalDate bd = LocalDate.now();
+    ContactInformation ci;
+
+    @BeforeEach
+    public void initialize() {
+
+        List<MedicalHistory> medicalHistoryList = new ArrayList<>();
+        medicalHistoryList.add(mh);
+
+        mh = new MedicalHistory("Fever", "Pills", "Dr House",
+                11111, startDate, null);
+
+        List<MedicalVisit> medicalVisitList = new ArrayList<>();
+        medicalVisitList.add(mv);
+
+        mv = new MedicalVisit("Montreal General Hospital",
+                "Dr House", 12345, visitDate,"Fever",
+                "Sleep", "High temperature 39 degree", "Happy");
+
+
+        ci = new ContactInformation(1333, "Boulevard Jacques Cartier E",
+                "Longueuil", "J4M2A5", "(450) 468-8111", "pb@qc.ca");
+
+        // Everything Valid
+        pf1 = new PatientFile("ABCD12345678", "Gregory", "House",
+                Gender.MALE, "Montreal", LocalDate.now(),
+                "John House, Mary Smith");
+        
+        // Invalid ramqcode
+        pf2 = new PatientFile("ABCD 12345678", "Gregory", "House",
+                Gender.MALE, "Montreal", LocalDate.of(2000, 1, 1),
+                "John House, Mary Smith");
+
+        // Invalid firstName
+        pf3 = new PatientFile("ABCD12345678", "Gregory 2", "House",
+                Gender.MALE, "Montreal", LocalDate.of(2000, 1, 1),
+                "John House, Mary Smith");
+
+        // Invalid lastName
+        pf4 = new PatientFile("ABCD12345678", "Gregory", "",
+                Gender.MALE, "Montreal", LocalDate.of(2000, 1, 1),
+                "John House, Mary Smith");
+
+        // Invalid birthCity
+        pf5 = new PatientFile("ABCD12345678", "Gregory", "House",
+                Gender.MALE, "Montréal", LocalDate.of(2000, 1, 1),
+                "John House, Mary Smith");
+
+        // Invalid knownParents
+        pf6 = new PatientFile("ABCD12345678", "Gregory", "House",
+                Gender.MALE, "Montreal", LocalDate.of(2000, 1, 1),
+                "");
+
+        // Invalid birthDate
+        pf7 =  new PatientFile("ABCD12345678", "Gregory", "House",
+                Gender.MALE, "Montreal", LocalDate.of(2000, 1, 1),
+                "John House, Mary Smith");
+    }
+
+    @Test public void validateRamqCode_Valid () {
+        assertEquals("ABCD12345678", pf1.getRamqCode());
+    }
+
+    @Test public void validateRamqCode_Invalid () {
+        assertEquals(null, pf2.getRamqCode());
+    }
+
+    @Test public void validateFirstName_Valid () {
+        assertEquals("Gregory", pf1.getFirstName());
+    }
+
+    @Test public void validateFirstName_Invalid () {
+        assertEquals(null, pf3.getFirstName());
+    }
+
+    @Test public void validateLastName_Valid () {
+        assertEquals("House", pf1.getLastName());
+    }
+
+    @Test public void validateLastName_Invalid () {
+        assertEquals(null, pf4.getLastName());
+    }
+
+    @Test public void validateBirthCity_Valid () {
+        assertEquals("Montreal", pf1.getBirthCity());
+    }
+
+    @Test public void validateBirthCity_Invalid () {
+        assertEquals(null, pf5.getBirthCity());
+    }
+
+    @Test public void validateKnownParent_Valid () {
+        assertEquals("John House, Mary Smith", pf1.getKnownParents());
+    }
+
+    @Test public void validateKnownParent_Invalid () {
+        assertEquals(null, pf6.getKnownParents());
+    }
+
+    @Test public void validateGender_Valid () {
+        assertEquals(Gender.MALE, pf1.getGender());
+    }
+
+    @Test public void validateBirthDate_Valid () {
+        assertEquals(bd, pf1.getBirthDate());
+    }
+
+    @Test public void createContactInformation() {
+        pf1.setContactInformation(ci);
+        assertEquals(1333, pf1.getContactInformation().getNumber());
+        assertEquals("Boulevard Jacques Cartier E", pf1.getContactInformation().getStreet());
+        assertEquals("Longueuil", pf1.getContactInformation().getCity());
+        assertEquals("J4M2A5", pf1.getContactInformation().getPostalCode());
+        assertEquals("(450) 468-8111", pf1.getContactInformation().getPhone());
+        assertEquals("pb@qc.ca", pf1.getContactInformation().getEmail());
+    }
+    @Test public void createMedicalVisit() {
+
+        assertNotNull(pf1.getMedicalVisits());
+
+    }
+
+    @Test public void createMedicalHistory() {
+
+        assertNotNull(pf1.getMedicalHistories());
+
+    }
+}
