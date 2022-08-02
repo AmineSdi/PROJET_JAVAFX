25fc88d59601a785f80f3d57111964d4a82c1a9a
Alexandre Filion <filion.alexandre.2@courrier.uqam.ca>
2022-08-02 14:06:57 -0400

    Mettre à jour la classe MedicalHistory
    Inclus les tests de cette classe

--- src/main/java/Model/PatientFile/MedicalHistory.java
+++ src/main/java/Model/PatientFile/MedicalHistory.java
@@ -8,12 +8,12 @@ import java.time.LocalDate;
 public class MedicalHistory implements Visitable {
     public MedicalHistory(String diagnosis, String treatment, String doctorName, int doctorLicense,
                           LocalDate startDate, LocalDate endDate) {
-        this.diagnosis = diagnosis;
-        this.treatment = treatment;
-        this.doctorName = doctorName;
-        this.doctorLicense = doctorLicense;
-        this.startDate = startDate;
-        this.endDate = endDate;
+        validateDiagnosis(diagnosis);
+        validateTreatment(treatment);
+        validateDoctorName(doctorName);
+        validateDoctorLicense(doctorLicense);
+        validateStartDate(startDate);
+        this.endDate = null;
     }
 
     String diagnosis;
@@ -27,48 +27,112 @@ public class MedicalHistory implements Visitable {
         return doctorLicense;
     }
 
-    public void setDoctorLicense(int doctorLicense) {
-        this.doctorLicense = doctorLicense;
+    public void validateDoctorLicense(int doctorLicense) {
+        // Valid DoctorLicense example : 12345
+        // Invalid DoctorLicense examples : 97-8514, 2541-08
+        if (doctorLicense >= 10000 && doctorLicense <= 99999)
+            this.doctorLicense = doctorLicense;
+        else
+            this.doctorLicense = 0;
     }
 
     public String getDiagnosis() {
         return diagnosis;
     }
 
-    public void setDiagnosis(String diagnosis) {
-        this.diagnosis = diagnosis;
+    public void validateDiagnosis(String diagnosis) {
+        // Valid diagnosis examples : Cancer, Cold + temperature 39
+        // Invalid diagnosis example : empty field
+        boolean isValid = false;
+        String validFormat = "^\\w+(\\s\\w+)*$";
+        if (diagnosis != null) {
+            if (diagnosis.matches(validFormat) && diagnosis.length() > 0)
+                isValid = true;
+
+            if (!isValid)
+                this.diagnosis = null;
+            else
+                this.diagnosis = diagnosis;
+        }
     }
 
     public String getTreatment() {
         return treatment;
     }
 
-    public void setTreatment(String treatment) {
-        this.treatment = treatment;
+    public void validateTreatment(String treatment) {
+        // Valid treatment examples : Sleep, pills
+        // Invalid treatment example : empty field
+        boolean isValid = false;
+        String validFormat = "^\\w+(\\s\\w+)*$";
+        if (treatment != null) {
+
+            if (treatment.matches(validFormat) && treatment.length() > 0)
+                isValid = true;
+
+            if (!isValid)
+                this.treatment = null;
+            else
+                this.treatment = treatment;
+        }
     }
 
     public String getDoctorName() {
         return doctorName;
     }
 
-    public void setDoctorName(String doctorName) {
-        this.doctorName = doctorName;
+    public void validateDoctorName(String doctorName) {
+        // Valid doctorName example : Dr House, Smith
+        // Invalid doctorName example : Dr House licence 852147
+        boolean isValid = false;
+        String validFormat = "^([a-zA-Z-\\s])*$";
+
+        if (doctorName.matches(validFormat) && doctorName.length() > 0)
+            isValid = true;
+
+        if (!isValid)
+            this.doctorName = null;
+        else
+            this.doctorName = doctorName;
     }
 
     public LocalDate getStartDate() {
         return startDate;
     }
 
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
+    public void validateStartDate(LocalDate startDate) {
+        // Valid visitDate example : today date
+        // Invalid phone examples : not today date
+        boolean isValid = false;
+
+        LocalDate date = LocalDate.now();
+        if (startDate.equals(date))
+            isValid = true;
+
+        if (!isValid)
+            this.startDate = null;
+        else
+            this.startDate = startDate;
     }
 
     public LocalDate getEndDate() {
         return endDate;
     }
 
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
+    public void validateEndDate(LocalDate endDate) {
+        // Valid visitDate example : any date after startDate or empty field
+        // Invalid phone examples : any date before startDate
+        // When creating a MedicalHistory, this should be null
+        boolean isValid = false;
+
+        LocalDate date = LocalDate.now();
+        if (endDate.equals(date))
+            isValid = true;
+
+        if (!isValid)
+            this.endDate = null;
+        else
+            this.endDate = endDate;
     }
 
     @Override
@@ -83,8 +147,8 @@ public class MedicalHistory implements Visitable {
      * TODO : Validators?
      * */
     public void modifyHistory(String diagnosis, String treatment, LocalDate endDate){
-        this.diagnosis = diagnosis;
-        this.treatment = treatment;
-        this.endDate = endDate;
+        validateDiagnosis(diagnosis);
+        validateTreatment(treatment);
+        validateEndDate(endDate);
     }
 }
---
+++ src/test/java/MedicalHistoryTest.java
@@ -0,0 +1,117 @@
+import Model.PatientFile.MedicalHistory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MedicalHistoryTest {
+
+    MedicalHistory mh1, mh2, mh3, mh4, mh5, mh6, mh7;
+    MedicalHistory mmh;
+
+
+    LocalDate startDate = LocalDate.now();
+    LocalDate endDate = LocalDate.now();
+    LocalDate startDate1 = LocalDate.of(2020, 3, 15);
+    //LocalDate endDate1 = LocalDate.of(2020, 3, 15);
+
+    @BeforeEach
+    public void initialize(){
+        // Everything Valid
+        mh1 = new MedicalHistory("Fever", "Pills", "Dr House",
+                11111, startDate, null);
+
+        // Everything Valid, but for modifyHistory tests
+        mmh = new MedicalHistory("Cold", "Sleep", "Dr Gibson",
+                55555, startDate, null);
+
+        // Changes for modifyHistory
+        mmh.modifyHistory("Cancer", "Chemotherapy", LocalDate.now());
+
+        // Invalid diagnosis
+        mh2 = new MedicalHistory("", "Pills", "Dr House",
+                11111, startDate, null);
+
+        // Invalid treatment
+        mh3 = new MedicalHistory("Fever", "", "Dr House",
+                11111, startDate, null);
+
+        // Invalid doctorName
+        mh4 = new MedicalHistory("Fever", "Pills", "",
+                11111, startDate, null);
+
+        // Invalid doctorLicence
+        mh5 = new MedicalHistory("Fever", "Pills", "Dr House",
+                100, startDate, null);
+
+        // Invalid startDate
+        mh6 = new MedicalHistory("Fever", "Pills", "Dr House",
+                11111, startDate1, null);
+
+        // Invalid endDate
+        mh7 = new MedicalHistory("Fever", "Pills", "Dr House",
+                11111, startDate, null);
+    }
+
+    @Test public void validateDiagnosis_Valid() {
+        assertEquals("Fever", mh1.getDiagnosis());
+    }
+
+    @Test public void validateDiagnosis_Invalid() {
+        assertEquals(null, mh2.getDiagnosis());
+    }
+
+    @Test public void validateTreatment_Valid() {
+        assertEquals("Pills", mh1.getTreatment());
+    }
+
+    @Test public void validateTreatment_Invalid() {
+        assertEquals(null, mh3.getTreatment());
+    }
+
+    @Test public void validateDoctorName_Valid() {
+        assertEquals("Dr House", mh1.getDoctorName());
+    }
+
+    @Test public void validateDoctorName_Invalid() {
+        assertEquals(null, mh4.getDoctorName());
+    }
+
+    @Test public void validateDoctorLicence_Valid() {
+        assertEquals(11111, mh1.getDoctorLicense());
+    }
+
+    @Test public void validateDoctorLicence_Invalid() {
+        assertEquals(0, mh5.getDoctorLicense());
+    }
+
+    @Test public void validateStartDate_Valid() {
+        assertEquals(startDate, mh1.getStartDate());
+    }
+
+    @Test public void validateStartDate_Invalid() {
+        assertEquals(null, mh6.getStartDate());
+    }
+
+    @Test public void validateEndDate_Valid() {
+        assertEquals(null, mh1.getEndDate());
+    }
+
+    @Test public void validateEndDate_Invalid() {
+        assertEquals(null, mh7.getEndDate());
+    }
+
+    @Test public void validateDiagnosisModify_Valid() {
+        assertEquals("Cancer", mmh.getDiagnosis());
+    }
+
+    @Test public void validateTreatmentModify_Valid() {
+        assertEquals("Chemotherapy", mmh.getTreatment());
+    }
+
+    @Test public void validateEndDateModify_Valid() {
+        assertEquals(LocalDate.now(), mmh.getEndDate());
+    }
+}
