f116e8e6cdc4e453abe035b4e9b39db92e2c00b8
Alexandre Filion <filion.alexandre.2@courrier.uqam.ca>
2022-08-04 15:32:13 -0400

    Corrige la classe Doctor
    Correction des regex et des tests

--- src/test/java/DoctorTest.java
+++ src/test/java/DoctorTest.java
@@ -15,10 +15,15 @@ public class DoctorTest {
 //    User user;
 //    HealthProfessional healthProfessional;
 //    Doctor doctor;
-    Doctor d1, d2, d3;
+    Doctor d1, d2, d3, doctor;
 
     MedicalEstablishment establishment;
     ContactInformation ci;
+    MedicalVisit mmv;
+    MedicalHistory mmh;
+
+    LocalDate startDate = LocalDate.now();
+    LocalDate visitDate = LocalDate.now();
 
     @BeforeEach
     public void initialize() {
@@ -33,6 +38,9 @@ public class DoctorTest {
         d1 = new Doctor(1, "Gregory", "House", "houseMD",
                         "aaa", 11111, "Internal Medicine", establishment);
 
+        doctor = new Doctor(1, "Gregory", "House", "houseMD",
+                "aaa", 11111, "Internal Medicine", establishment);
+
         // Invalid Doctor License
         d2 = new Doctor(1, "Gregory", "House", "houseMD",
                         "aaa", 1, "Internal Medicine", establishment);
@@ -41,52 +49,68 @@ public class DoctorTest {
         d3 = new Doctor(1, "Gregory", "House", "houseMD",
                 "aaa", 11111, "", establishment);
 
+        // Everything Valid, but for modifyVisit tests
+        mmv = new MedicalVisit("CHUM", "Dr Gibson", 55555,
+                visitDate,"Heart problem.", "Pill.",
+                "Problem with the heart.", "Visit later.");
+
+        // Changes for modifyVisit
+        mmv.modifyVisit("CHUM", "Gibson","Smith", 25252,
+                LocalDate.now(), "No cancer.", "Nothing to do.",
+                "Patent is cancer free.", "To follow in 2 months.");
+
+        // Everything Valid, but for modifyHistory tests
+        mmh = new MedicalHistory("Cold", "Sleep", "Dr Gibson",
+                55555, startDate, null);
+
+        // Changes for modifyHistory
+        mmh.modifyHistory("Now has cancer.", "Chemotherapy?", LocalDate.now());
     }
 
-//    @Test public void validateLicenseTest_Valid() {
-//        assertEquals(11111, d1.getLicense());
-//    }
-//
-//    @Test public void validateLicenseTest_Invalid() {
-//        assertEquals(0, d2.getLicense());
-//    }
-//
-//    @Test public void validateSpecialtyTest_Valid() {
-//        assertEquals("Internal Medicine", d1.getSpecialty());
-//    }
-//
-//    @Test public void validateSpecialtyTest_Invalid() {
-//        assertEquals(null, d3.getSpecialty());
-//    }
+    @Test public void validateLicenseTest_Valid() {
+        assertEquals(11111, d1.getLicense());
+    }
 
-//    @Test public void createVisitTest() {
-//        MedicalVisit mv = d1.createVisit();
-//
-//        assertEquals("Pierre-Boucher Hospital", mv.getEstablishmentName());
-//        assertEquals("Gregory House", mv.getDoctorName());
-//        assertEquals(11111, mv.getDoctorLicense());
-//        assertEquals(LocalDate.now(), mv.getVisitDate());
-//
-//        assertEquals(null, mv.getDiagnosis());
-//        assertEquals(null, mv.getTreatment());
-//        assertEquals(null, mv.getVisitSummary());
-//        assertEquals(null, mv.getNotes());
-//    }
-//
-//    @Test public void createHistoryTest() {
-//        MedicalHistory mh = d1.createHistory();
-//        assertEquals(null, mh.getDiagnosis());
-//        assertEquals(null, mh.getTreatment());
-//        assertEquals("Gregory House", mh.getDoctorName());
-//        assertEquals(11111, mh.getDoctorLicense());
-//        assertEquals(LocalDate.now(), mh.getStartDate());
-//        assertEquals(null, mh.getEndDate());
-//    }
-//
-//    @Test public void medicalEstablishment_Valid() {
-//        assertEquals("Pierre-Boucher Hospital", d1.getEstablishmentName());
-//
-//    }
+    @Test public void validateLicenseTest_Invalid() {
+        assertEquals(0, d2.getLicense());
+    }
+
+    @Test public void validateSpecialtyTest_Valid() {
+        assertEquals("Internal Medicine", d1.getSpecialty());
+    }
+
+    @Test public void validateSpecialtyTest_Invalid() {
+        assertEquals(null, d3.getSpecialty());
+    }
+
+    @Test public void createVisitTest() {
+        MedicalVisit mv = d1.createVisit();
+
+        assertEquals("Pierre-Boucher Hospital", mv.getEstablishmentName());
+        assertEquals("Gregory House", mv.getDoctorName());
+        assertEquals(11111, mv.getDoctorLicense());
+        assertEquals(LocalDate.now(), mv.getVisitDate());
+
+        assertEquals(null, mv.getDiagnosis());
+        assertEquals(null, mv.getTreatment());
+        assertEquals(null, mv.getVisitSummary());
+        assertEquals(null, mv.getNotes());
+    }
+
+    @Test public void createHistoryTest() {
+        MedicalHistory mh = d1.createHistory();
+        assertEquals(null, mh.getDiagnosis());
+        assertEquals(null, mh.getTreatment());
+        assertEquals("Gregory House", mh.getDoctorName());
+        assertEquals(11111, mh.getDoctorLicense());
+        assertEquals(LocalDate.now(), mh.getStartDate());
+        assertEquals(null, mh.getEndDate());
+    }
+
+    @Test public void medicalEstablishment_Valid() {
+        assertEquals("Pierre-Boucher Hospital", d1.getEstablishmentName());
+
+    }
 
 }
 
